2b95976179b8be5b62e5e37a2fed35f505af7e5c
Kristen Manning <kristenm@nat-oitwireless-inside-vapornet100-a-24916.Princeton.EDU>
2017-03-22 12:48:38 -0400

    adding all() method to ORM

--- config/orm.js
+++ config/orm.js
@@ -1,14 +1,19 @@
-// // Import MySQL connection.
-// var connection = require("../config/connection.js");
+// Import MySQL connection.
+var connection = require("../config/connection.js");
 
-// var orm  = {
+var orm  = {
 
-// 	all: function(table, cb){
-// 		var querStr = "SELECT * "
+	all: function(table, cb){
+		var querStr = "SELECT * FROM " + table + "; "; 
+		connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+	    cb(result);
 
-// 	}
+		});
+	}, 
+}
 
-// }
 
-
-// module.exports = orm;
+module.exports = orm;

29006f7951f6ab8bb42ae153e10a27103089e7bc
Kristen Manning <kristenm@nat-oitwireless-inside-vapornet100-a-24916.Princeton.EDU>
2017-03-22 12:44:02 -0400

    created schema and seeds

---
+++ config/orm.js
@@ -0,0 +1,14 @@
+// // Import MySQL connection.
+// var connection = require("../config/connection.js");
+
+// var orm  = {
+
+// 	all: function(table, cb){
+// 		var querStr = "SELECT * "
+
+// 	}
+
+// }
+
+
+// module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+USE burgers_db; 
+INSERT INTO burgers (burger_name) VALUES ('The works');
+INSERT INTO burgers (burger_name) VALUES ('Vegetarian');
